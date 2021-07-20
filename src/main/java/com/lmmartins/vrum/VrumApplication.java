78d431d4de93d76a2f6ab30b49bffdc11deadc9e
pdpan0 <lm.m4rtins@gmail.com>
2021-07-19 22:23:16 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/com/lmmartins/vrum/VrumApplication.java
@@ -0,0 +1,13 @@
+package com.lmmartins.vrum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VrumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lmmartins/vrum/VrumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmmartins.vrum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VrumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
