42d22e2986a7764005efe6e660d95ad80159f218
pdpan0 <lm.m4rtins@gmail.com>
2021-07-26 17:30:03 -0300

    Alterando CascadeType

--- src/main/java/com/lmmartins/vrum/models/Motorista.java
+++ src/main/java/com/lmmartins/vrum/models/Motorista.java
@@ -3,9 +3,12 @@ package com.lmmartins.vrum.models;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.List;
 
 @Getter
 @Setter
@@ -19,6 +22,8 @@ public class Motorista extends Pessoa {
     private String modeloCarro;
     @Column(nullable = false)
     private Boolean status;
+    @OneToMany(mappedBy = "motorista", orphanRemoval = true, cascade = CascadeType.PERSIST)
+    private List<Corrida> corridas;
 
     public Motorista(Long id,
                      String nome,
--- src/main/java/com/lmmartins/vrum/models/Passageiro.java
+++ src/main/java/com/lmmartins/vrum/models/Passageiro.java
@@ -3,12 +3,13 @@ package com.lmmartins.vrum.models;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.List;
 
 @Getter
 @Setter
@@ -18,6 +19,8 @@ public class Passageiro extends Pessoa {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @OneToMany(mappedBy = "passageiro", orphanRemoval = true, cascade = CascadeType.PERSIST)
+    private List<Corrida> corridas;
 
     public Passageiro(Long id,
                       String nome,
